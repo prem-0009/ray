f7f92e11a58347b8e6da42f0ab5b78640d955b28
prem <prem.sherpa@codeimmersives.com>
2020-07-29 15:02:21 -0400

    need work---

--- main.js
+++ main.js
@@ -1,6 +1,50 @@
-const Ray = function() {
+const Ray = function () {
+    return {
+      length: 0,
+      push: function (value) {
+        this[this.length] = value;
+        this.length ++;
+      },
+      pop: function(){
+          const toBeRemoved = this[this.length-1]
+          delete this[this.length-1]
   
-}
-
-
-module.exports = Ray
+          this.length --;
+          return toBeRemoved;
+      },
+      includes: function (value) {
+        for (let i = 0; i < this.length; i++) {
+      
+          if (value === this[i]) {//this[i]????
+            return true;
+          } else {
+            return false;
+          }
+        }
+      },
+      indexOf: function(str){
+          for ( let i = 0; i < this.length; i++){
+              if ( str === this[i]){
+                  return i
+              }
+          }
+  
+          return -1;
+      },
+      shift: function(value){
+          // console.log(value)
+          
+          let toShift = this[0];
+          console.log(toShift)
+          delete this[0];
+          this.length --;
+          return toShift;
+          
+      }
+  
+  
+    };
+  };
+  
+  module.exports = Ray;
+  
